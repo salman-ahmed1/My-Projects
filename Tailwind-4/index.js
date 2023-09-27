be95aa4c64233db7f1ee7fcce526e37dacae31d2
salman-ahmed1 <58296578+salman-ahmed1@users.noreply.github.com>
2023-09-28 01:41:39 +0600

    button function added

---
+++ Tailwind-4/index.js
@@ -0,0 +1,6 @@
+$("#hamburger").click(function () {
+  $("#menu").toggleClass("block");
+  $("#menu").toggleClass("hidden");
+  $("#menu").toggleClass("exist");
+  $(".menu").toggleClass("animIn");
+});
